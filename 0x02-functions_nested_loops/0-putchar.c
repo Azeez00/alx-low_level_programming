ef04cab8d9c56f13e4a73391b6d1513760505f6f
Azeez00 <azeezabdullah7@gmail.com>
2022-03-15 04:55:38 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "_putchar.h"
+
+/**
+ * main - Prints _putchar
+ * @void: Empty parameter list for main.
+ *
+ * Description: Prints _putchar w/out stdio
+ *
+ * Return: 0 for success
+*/
+int main(void)
+{
+	_putchar('a');
+	_putchar('l');
+	_putchar('x');
+	_putchar('e');
+	_putchar('n');
+	_putchar('g');
+	_putchar('i');
+	_putchar('n');
+	_putchar('e');
+	_putchar('\n');
+	return (0);
+}
